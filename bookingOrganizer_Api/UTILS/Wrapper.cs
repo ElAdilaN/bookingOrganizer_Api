59d7225aa59423b5b3b87460ddfc78b7e716c192
Noureddin El Adila <noureddineladila@gmail.com>
2025-05-20 12:34:15 +0200

    feat : create the Wrapper class

---
+++ bookingOrganizer_Api/UTILS/Wrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace bookingOrganizer_Api.UTILS
+{
+    public class Wrapper
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+        public Dictionary<string, object> data { get; set; }
+    }
+}
