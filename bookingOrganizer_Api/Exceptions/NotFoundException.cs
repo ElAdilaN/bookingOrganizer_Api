84bb037f8e476ebc7cf11a9a702d20449582a536
Noureddin El Adila <noureddineladila@gmail.com>
2025-05-22 12:08:59 +0200

    feat : create NotFoundException

---
+++ bookingOrganizer_Api/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace bookingOrganizer_Api.Exceptions
+{
+    public class NotFoundException:AppException
+    {
+        public NotFoundException() { }
+
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
