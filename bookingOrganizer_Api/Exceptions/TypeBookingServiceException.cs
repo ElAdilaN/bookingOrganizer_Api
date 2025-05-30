dbe6202cd7604dd5736b29eb27c2bae3961a0a2e
Noureddin El Adila <noureddineladila@gmail.com>
2025-05-30 11:13:32 +0200

    feat(TypeBooking): create the typeBooking Service Exception

---
+++ bookingOrganizer_Api/Exceptions/TypeBookingServiceException.cs
@@ -0,0 +1,10 @@
+﻿namespace bookingOrganizer_Api.Exceptions
+{
+    public class TypeBookingServiceException : AppException
+    {
+        public TypeBookingServiceException(string message) : base(message) { }
+
+        public TypeBookingServiceException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+}
