00bfd4faad9803bece7f5ae631454f78bd838b4b
Noureddin El Adila <noureddineladila@gmail.com>
2025-05-30 23:59:16 +0200

    feat(IDAO- GROUP ): create the interface of group , and implement it on DAOGroup

---
+++ bookingOrganizer_Api/IDAO/IDAOGroup.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using bookingOrganizer_Api.Models;
+namespace bookingOrganizer_Api.IDAO
+{
+    public interface  IDAOGroup
+    {
+        public ICollection<Models.Group> getAllGroups();
+        public Models.Group getGroupById(int groupId);
+
+        public void addGroup(Models.Group group);
+
+        public void RemoveGroup(int groupId);
+        public Task UpdateGroup(Models.Group updatedGroup);
+
+    }
+}
