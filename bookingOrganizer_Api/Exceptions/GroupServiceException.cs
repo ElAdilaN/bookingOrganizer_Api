7b0f162cd61ada8c81c48a6b51aadf8321739d72
Noureddin El Adila <noureddineladila@gmail.com>
2025-05-31 21:40:20 +0200

    feat( Exception - Group): create the Group Service Exception

---
+++ bookingOrganizer_Api/Exceptions/GroupServiceException.cs
@@ -0,0 +1,8 @@
+﻿namespace bookingOrganizer_Api.Exceptions
+{
+    public class GroupServiceException : AppException
+    {         
+        public GroupServiceException(string message) : base(message) { }
+        public GroupServiceException(string message, Exception innerException) : base(message, innerException) { }        
+    }
+}
