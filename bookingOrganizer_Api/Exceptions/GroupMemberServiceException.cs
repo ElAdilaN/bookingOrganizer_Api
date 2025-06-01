25de5be7b69c75a12281c6b2c491dae5f9e154ea
Noureddin El Adila <noureddineladila@gmail.com>
2025-06-01 23:06:59 +0200

    feat(Exception - GroupMember): createthe grupMember Service Exception Hanlder

---
+++ bookingOrganizer_Api/Exceptions/GroupMemberServiceException.cs
@@ -0,0 +1,9 @@
+﻿namespace bookingOrganizer_Api.Exceptions
+{
+    public class GroupMemberServiceException : AppException
+    {
+        public GroupMemberServiceException(string message) : base(message) { }
+        public GroupMemberServiceException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+}
